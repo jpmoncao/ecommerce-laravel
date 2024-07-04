5933181c3929d5f37a8098e880e22c2ee5becab4
jpmoncao <pedrohjoao44@gmail.com>
2024-07-04 16:03:04 -0300

    feat: model dos produtos e das variações

---
+++ database/migrations/2024_07_04_185950_create_product_variations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variations', function (Blueprint $table) {
+            $table->string('id_product_variation', 36)->primary();
+            $table->string('variation');
+            $table->float('amount', 2);
+            $table->integer('product_id');
+            $table->foreign('product_id')->references('id_product')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variations');
+    }
+};
