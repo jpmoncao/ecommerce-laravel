f81cde74139438ecb84538e1bf85ae802985f711
jpmoncao <pedrohjoao44@gmail.com>
2024-08-23 15:01:54 -0300

    feat: criação do método de inserção de págamento

--- ecommerce/app/Http/Controllers/PaymentsController.php
+++ ecommerce/app/Http/Controllers/PaymentsController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Payments;
+use App\Models\Financials;
+use App\Models\Orders;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Utils\ValidatorRequest;
 
 class PaymentsController extends Controller
 {
@@ -28,7 +32,51 @@ class PaymentsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Cria objeto de validação
+        $validate = new ValidatorRequest($request, [
+            'financial_id' => 'required|integer',
+            'amount' => 'required|float|min:0.01',
+            'method' => 'required|string',
+            'discount' => 'required|float|min:0.01',
+            'surcharge' => 'required|float|min:0.01',
+        ]);
+
+        // Valida se os dados batem com a validação, caso não, dispara erro
+        $error = $validate->handleErrors();
+        if ($error)
+            return $error;
+
+        // Obtém o financeiro pelo id
+        $financial = Financials::where('id_financial', $request->financial_id);
+
+        // Caso não encontre, dispara mensagem de não encontrado
+        if (!$financial)
+            return response()->json(['message' => 'Financial not found!'], 404);
+
+        // Obtém os valores do financeiro
+        $financialPaidAmount = $financial->paid_amount();
+        $financialTotalAmount = $financial->total_amount();
+
+        // Se o valor do pagamento mais o valor pago for maior que o total do financeiro, dispara erro
+        if ($financialPaidAmount + $request->amount > $financialTotalAmount)
+            return response()->json(['message' => 'The payment amount will exceed the total to be paid!'], 403);
+
+        // Cria pagamento
+        $atributtes = [
+            'financial_id' => $request->financial_id,
+            'amount' => $request->amount,
+            'method' => $request->method,
+            'discount' => $request->discount,
+            'surchage' => $request->surchage,
+            'sequencial' => $financial->lastPaymentSequencial() + 1
+        ];
+        $payment = Payments::create($atributtes);
+
+        // Retorna pagamento com mensagem de sucesso
+        return response()->json([
+            'message' => 'Payment created successfully!',
+            'data' => $payment
+        ], 201);
     }
 
     /**
