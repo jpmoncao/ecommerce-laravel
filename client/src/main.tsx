f3eb2adaa42b1a19e62c75bb32fe0a8c8c0ab3b1
jpmoncao <pedrohjoao44@gmail.com>
2024-09-13 19:25:44 -0300

    feat: adicionando shad cn, tailwind, react com vite

---
+++ client/src/main.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+import { router } from './router/routes';
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ client/src/router/routes.tsx
@@ -0,0 +1,11 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+
+import Home from '@/pages/home';
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+]);
