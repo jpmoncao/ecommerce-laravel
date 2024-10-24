a4b041d16190048961c80a06f24354a764ec66b5
jpmoncao <pedrohjoao44@gmail.com>
2024-10-24 13:37:23 -0300

    docs: middleware para criar usuário temporário

--- ecommerce/app/Http/Middleware/CheckTemporaryUser.php
+++ ecommerce/app/Http/Middleware/CheckTemporaryUser.php
@@ -20,8 +20,6 @@ class CheckTemporaryUser
      */
     public function handle(Request $request, Closure $next)
     {
-        Log::info('', ['Auth::check()' => Auth::check()]);
-
         if (!Auth::check()) {
             // Se não tiver um usuário autenticado, cria um novo usuário anônimo
             if (!session()->has('temp_user_id')) {
@@ -35,28 +33,23 @@ class CheckTemporaryUser
 
                 // Faz o login com o usuário temporário
                 Auth::login($user, true);
-
-                Log::info('Nenhum usuário autenticado, criando um novo usuário anônimo.', ['user_id' => $user->id_user]);
             } else {
                 // Recupera o usuário temporário da sessão, se houver
                 $tempUserId = session('temp_user_id');
                 $user = User::find($tempUserId);
 
-                if ($user) {
-                    // Loga novamente o usuário temporário
-                    Auth::login($user, true);
-                    Log::info('Usuário temporário logado novamente.', ['user_id' => $user->id_user]);
-                } else {
+                if (!$user) {
                     // Caso o usuário da sessão não exista mais, cria um novo
                     $user = User::create([
                         'id_user' => (string) Str::uuid(),
                     ]);
 
+                    // Cria sessão com usuário temporário
                     session(['temp_user_id' => $user->id_user]);
-                    Auth::login($user, true);
-
-                    Log::info('Criando novo usuário temporário porque o usuário da sessão não foi encontrado.', ['user_id' => $user->id_user]);
                 }
+
+                // Autentifica o usuário
+                Auth::login($user, true);
             }
         }
 
